3d88d9e3d0abd23e9f02037343a4be761c160584
YutaKakiki <uta31501224@gmail.com>
2026-07-06 01:37:32 +0900

    docs: add package comment to turbo

--- turbo/doc.go
+++ turbo/doc.go
@@ -1,2 +1,8 @@
-// Package turbo provides ...
+// Package turbo provides server-side helpers for building web
+// applications that use Hotwire's Turbo runtime (Drive, Frames, Streams)
+// with Go and html/template.
+//
+// The package is a thin server-side layer: it does not ship any of the
+// Turbo JavaScript itself, and does not attempt to replace an HTTP mux
+// or web framework.
 package turbo
