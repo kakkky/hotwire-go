6e49f45b3094375ddf1a3bf728619665718144c7
YutaKakiki <uta31501224@gmail.com>
2026-07-06 01:35:55 +0900

    docs: add package comment to templatefunc

---
+++ turbo/internal/template-func/doc.go
@@ -0,0 +1,5 @@
+// Package templatefunc implements the html/template helpers exposed by
+// the parent turbo package. It is an internal package and its symbols
+// are not part of the public API; consumers should reach these helpers
+// by registering turbo.TemplateFuncMap on their template.
+package templatefunc
